6b4a61081e0b54d0032cad0814a37424c57641c9
sgc93 <smachewgedefaw@gmail.com>
2022-12-08 15:23:58 -0800

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
-#include <stdio.h>
+#include<stdio.h>
 /**
  * main - is the entry for the execution
  * Return must be always 0
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+        puts("\"Programming is like building a multilingual puzzle");
+        return (0);
 }
