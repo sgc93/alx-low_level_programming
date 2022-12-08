09d66b7515531dc1ce70c86aa5a86d6a217add9e
sgc93 <smachewgedefaw@gmail.com>
2022-12-08 15:55:50 -0800

    Hello world

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
-
 /**
  * main - entry
- * Return : always 0
+ * Return: always 0
  */
 
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
-
 }
