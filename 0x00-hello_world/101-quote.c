84586e2f3e8b5a2d5f36de698c377c899025624a
sgc93 <smachewgedefaw@gmail.com>
2022-12-09 02:58:38 -0800

    UNIX is basically a simple operating system, but you have to be a genius to understand the simplicity

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -11,6 +11,6 @@ int main(void)
 	long fd = 1;
 	long syscall = 1;
 	long ret = 0;
-	__asm__ ("syscall": "=a" (ret): "a" (syscall), "D" (fd), "S" (s), "d" (l));
+	__asm__ ("syscall" : "=a" (ret) : "a" (syscall), "D" (fd), "S" (s), "d" (l));
 	return (1);
 }
