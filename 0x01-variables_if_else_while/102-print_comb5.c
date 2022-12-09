d96412f222efc96febdb0884cb884fc59768df88
sgc93 <smachewgedefaw@gmail.com>
2022-12-09 07:47:10 -0800

    Priject 2, Task 12

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -10,7 +10,7 @@ int main(void)
 	int first_nums;
 	int second_nums;
 
-	
+
 	for (first_nums = 0; first_nums < 100 ; first_nums++)
 	{
 		for (second_nums = first_nums + 1 ; second_nums < 100; second_nums++)
