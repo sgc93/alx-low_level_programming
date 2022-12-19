7c8b77b909a16225c416eba2704e34b7058b1464
sgc93 <smachewgedefaw@gmail.com>
2022-12-18 23:18:41 -0800

    98 Battery st.

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,23 +1,16 @@
 #include "main.h"
-
 /**
  *
- * *  reset_to_98 - takes the pointer and updates the value
+ * reset_to_98 - takes the pointer and updates the value
  *
- * *  @n: int to check
+ * @n: int to check
  *
- * *  Owned By Bwave/Bright
+ * Owned By Bwave/Bright
  *
- * *  Return: 0 is success
- *
- * */
-
-
+ * Return: 0 is success
+ */
 
 void reset_to_98(int *n)
-
 {
-
 		*n = 98;
-
 }
