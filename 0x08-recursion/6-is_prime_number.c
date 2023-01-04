d43b90eadbec0405b9fe6f22e856a5c89be517f8
sgc93 <smachewgedefaw@gmail.com>
2023-01-04 07:34:33 -0800

    6. Inception. Is it possible?

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -28,3 +28,30 @@ return (evaluate_num(num, iterator + 1));
 return (0);
 
 }
+
+/**
+ *is_prime_number - evaluate prime or not
+ *@num: number
+ *Return: return 1 prime - return 0 otherwise
+ */
+
+int is_prime_number(int num)
+{
+
+int iterator;
+
+iterator = 2;
+
+/* only greater than 2*/
+if (num < 2)
+{
+return (0);
+}
+
+if (num == 2)
+{
+return (1);
+}
+
+return (evaluate_num(num, iterator));
+}
