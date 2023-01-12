b5b9f48e753d9362dbd5e23c3e134db078d8e0b3
sgc93 <smachewgedefaw@gmail.com>
2023-01-12 08:47:12 -0800

    task 5 is updated

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -8,22 +8,26 @@
  */
 int main(int argc, char *argv[])
 {
-unsigned long mul;
-int i, j;
+	unsigned long mul;
+	int i, j;
+
 	if (argc != 3)
-	{ printf("Error\n");
-	exit(98); }
+	{ 
+		printf("Error\n");
+		exit(98); 
+	}
 	for (i = 1; i < argc; i++)
 	{
 		for (j = 0; argv[i][j] != '\0'; j++)
 		{
 			if (argv[i][j] > 57 || argv[i][j] < 48)
-			{  printf("Error\n");
-			exit(98); }
+			{
+				printf("Error\n");
+				exit(98);
+			}
 		}
-
 	}
 	mul = atol(argv[1]) *atol(argv[2]);
 	printf("%lu\n", mul);
-return (0);
+	return (0);
 }
